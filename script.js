7b32464b0f9e26d253a3ccf28a0d3188794b4d71
Beebarh.jy <barbex.jy@gmail.com>
2025-11-06 11:56:28 +0100

    Initial commit of portfolio project

---
+++ script.js
@@ -0,0 +1,31 @@
+//Initialize AOS 
+AOS.init({
+    duration: 1000,
+    once: true
+})
+
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+
+//Dark Theme initializer
+//const toggle = document.getElementById('theme-toggle');
+//toggle.addEventListener('click', (event) => {
+//event.preventDefault();
+//document.documentElement.classList.toggle('dark-theme');
+//});
+
+const toggle = document.getElementById('theme-toggle');
+toggle.addEventListener('click', (event) => {
+    event.preventDefault(); // stops the link from reloading the page
+    const currentTheme = document.documentElement.getAttribute('dark-theme');
+    document.documentElement.setAttribute(
+        'data-theme',
+        currentTheme === 'green' ? 'default' : 'green'
+    );
+});
